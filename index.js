80d1f3aaa3cf03f44d43831cd494a73552f03706
Milan Sacher <milan.lanlada@gmail.com>
2018-06-27 04:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
-  recipes[key] = value
+  recipes[key] = value;
   
   return recipes
 }
-
-function deleteFromObjectByKey(recipes, key) {
-  delete recipes.key;
-  
-  return recipes
-}
-
